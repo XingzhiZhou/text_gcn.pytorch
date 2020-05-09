9612448237a8f2d8745cc5a8739aa912d166c815
Jason Zhou <Xingzhi06Zhou@gmail.com>
2020-05-09 20:03:24 +0800

    add explanation on config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class CONFIG(object):
         super(CONFIG, self).__init__()
         
         self.dataset = 'R8'
-        self.model = 'gin'  # 'gcn', 'gcn_cheby', 'dense'
+        self.model = 'gin'  #dataset 'gcn', 'gcn_cheby', 'dense', 'gin'
         self.learning_rate = 0.02   # Initial learning rate.
         self.epochs  = 300  # Number of epochs to train.
         self.hidden1 = 200  # Number of units in hidden layer 1.
@@ -15,11 +15,11 @@ class CONFIG(object):
         self.early_stopping = 80 # Tolerance for early stopping (# of epochs).
         self.max_degree = 3      # Maximum Chebyshev polynomial degree.
 
-        self.num_layers = 2
-        self.num_mlp_layers = 1
-        self.embed_dim = 200
-        self.hidden_dim_mlp = 200
-        self.train_eps = False
+        self.num_layers = 2  # Number of layers in GIN
+        self.num_mlp_layers = 1 #Number of layers in mlp
+        self.embed_dim = 200 # embedding dimension
+        self.hidden_dim_mlp = 200 # hidden layer dimension in mlp
+        self.train_eps = False # decide whether include eps or not
 
 
 
